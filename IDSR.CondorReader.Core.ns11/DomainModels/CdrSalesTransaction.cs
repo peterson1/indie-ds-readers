04fb31b98acb6dfeb1897728d2e614621736eeed
Pete @BK1-SvrA <petersonsalamat@gmail.com>
2017-06-27 17:56:39 +0800

    Added ToString method.

--- IDSR.CondorReader.Core.ns11/DomainModels/CdrSalesTransaction.cs
+++ IDSR.CondorReader.Core.ns11/DomainModels/CdrSalesTransaction.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace IDSR.CondorReader.Core.ns11.DomainModels
 {
@@ -8,5 +9,13 @@ namespace IDSR.CondorReader.Core.ns11.DomainModels
         public List<CdrTransactionLine>     Lines     { get; set; }
         public List<CdrTransactionLine>     Returns   { get; set; }
         public List<CdrTransactionPayment>  Payments  { get; set; }
+
+
+        public override string ToString()
+            => $"POS Transaction:{Header?.TransactionNo}"
+                        + $" Terminal:{Header?.TerminalNo}"
+                        + $" Lines:{Lines?.Count}"
+                        + $" Payments:{Payments?.Count}"
+                        + $" P{this.Payments.Sum(x => x.Amount):N0}";
     }
 }
